90e5f3e03ecbf17ef793f915da59c35b7305761a
WOJTOP_PC\48690 <wojtop@interia.pl>
2025-07-18 11:27:42 +0200

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+
 import './App.css'
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 import Descriptor from './components/Descriptor.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>
+      <main className="flex-grow">
         <Descriptor />
       </main>
-      
-    </>
+      <Footer />
+    </div>
   )
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-gray-100 mt-auto border-t border-gray-300 shadow-inner">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex flex-col sm:flex-row justify-between items-center text-sm text-gray-600">
+        
+        <div className="mb-2 sm:mb-0 italic font-medium tracking-wide">
+          Polska Grupa Biogazowa
+        </div>
+
+        <div className="text-center sm:text-right">
+          © {new Date().getFullYear()} Stronę napisał:{" "}
+          <a
+            href="https://github.com/Wojtoppin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 font-semibold hover:underline"
+          >
+            Wojciech Polit
+          </a>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
